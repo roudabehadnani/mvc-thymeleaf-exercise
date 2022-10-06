8a9884600b2b626f442056bd93302c4781de80d4
roudabehadnani <rodabeh.adnani@gmail.com>
2022-10-06 12:07:12 +0200

    Initial commit

---
+++ src/test/java/com/example/mvcthymeleafexercise/MvcThymeleafExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvcthymeleafexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcThymeleafExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
